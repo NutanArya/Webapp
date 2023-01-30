097fb5e0c1acc35f8138599af2e4d105e349f9a6
NutanArya <99880109+NutanArya@users.noreply.github.com>
2023-01-30 06:37:57 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import ".//style.css";
+import logo from "./logo.png";
+import React, { useState } from "react";
+
+
+function App() {
+  const [users, setUsers] = useState();
+  const getUsers = async () => {
+    const response = await fetch("https://reqres.in/api/users?");
+    const data = await response.json();
+    setUsers(data.data);
+  };
+
+  return (
+    <>
+      <header className="app">
+        <img className="lgm" src={logo} alt="cafe Coffe club" />
+        <h1> Cafe Coffee Club</h1>
+        <div className="btn" onClick={getUsers}>
+          Fetch Users
+        </div>
+      </header>
+      <div className="row">
+        {users?.map((current, index) => {
+          return (
+            <div className="card column" key={index}>
+              <img src={current.avatar} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h3 className="card-title">
+                  {current.first_name} {current.last_name}
+                </h3>
+                <h4 className="card-text">{current.email}</h4>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+}
+export default App;
